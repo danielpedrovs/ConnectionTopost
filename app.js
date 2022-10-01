2cc7f1e64d3342475ab00a28e4aea381e8010d62
danielpedrovs <danielpedrovs@gmail.com>
2022-10-01 01:07:44 +0100

    first commit of Program of Post

---
+++ app.js
@@ -0,0 +1,9 @@
+const app=Vue.createApp({
+    data(){
+        return{
+            courseGoal:'Finish the course and learn vue!'
+            vueLink:'https://vuejs.org/'
+        };
+    }
+});
+app.mount('#user-goal');
