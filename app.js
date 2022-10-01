b558ff7a1fd27183d77597a0ce87e08943c90328
danielpedrovs <danielpedrovs@gmail.com>
2022-10-01 17:41:48 +0100

    change some formulas to display random mensages

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
     const app=Vue.createApp({
     data(){
         return{
-            courseGoal:'Finish the course and learn vue!',
+            courseGoalA:'Finish the course and learn vue!',
+            courseGoalB:'Master in vue and build amazing apps',
             vueLink:'https://vuejs.org/'                                                                                                                                
         };
-    }
-});
+    },
+    methods:{
+        outputGoal(){
+            const randomNumber = Math.random();
+            if ( randomNumber <0.5){
+                return this.courseGoalA;
+            }else{
+                return 'Master Vue!';
+            }
+            }
+        }
+    });
 app.mount('#user-goal');
